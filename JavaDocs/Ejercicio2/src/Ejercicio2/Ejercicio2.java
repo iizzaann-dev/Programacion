b67c5dd208ae1d9e125099fc345291ebbe9aec81
Izan Rico <iricbla2402@g.educaand.es>
2025-09-26 15:59:27 +0200

    “Ejercicios_y_ejemplos_de_la_primera_práctica_de_Programación

---
+++ JavaDocs/Ejercicio2/src/Ejercicio2/Ejercicio2.java
@@ -0,0 +1,25 @@
+/*
+ * Descripción: Realizar un programa que muestre en pantalla cierta información
+ * Autor: Izan Rico Blanco
+ * Fecha: 24/09/2025
+ */
+
+package Ejercicio2;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Desarrollo de Aplicaciones Web/Multiplataforma"); /*En esta y las siguientes líneas conseguimos que se imprima en pantalla la información
+		presente en las comillas*/
+		
+		System.out.println("1DAW /DAM");
+		
+		System.out.println("Módulos:");
+		
+		System.out.println("PRO");
+		
+		System.out.println("SIS");
+
+	}
+
+}
---
+++ JavaDocs/Ejercicio3/src/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,42 @@
+/*
+ * Descripción: Crear e inicializar variables
+ * 
+ * Autor: Izan Rico Blanco
+ * 
+ * Fecha: 24/09/2025
+ */
+
+package Ejercicio3;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		//Declaramos el valor de las variables
+		int num1 = 0;  
+		
+		int num2 = 0; 
+		
+		double val1 = 2.4;  
+		
+		double val2 = 0.7;  
+		
+		String nombre = "Izan"; 
+		
+		String apellidos = "Rico Blanco";  
+		
+		//Imprimimos en pantalla las variables solicitadas
+		
+		System.out.println ("Estos son los dos números enteros que he elegido: " + num1 + " y " + num2);
+		
+		System.out.println ("Estos son los dos números decimales que he elegido: " + val1 + " y " + val2 );
+		
+		System.out.print ("Mi nombre es: " + nombre + " y estos son mis apellidos: " + apellidos);
+		
+		
+		
+		
+
+	}
+
+}
