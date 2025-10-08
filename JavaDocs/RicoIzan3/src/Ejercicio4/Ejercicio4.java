ba75fdf5bf441636acc4a960c61179cca751f618
Izan <iricbla2402@g.educaand.es>
2025-10-08 18:15:15 +0200

    A punto de terminar la practica 3

---
+++ JavaDocs/RicoIzan3/src/Ejercicio4/Ejercicio4.java
@@ -0,0 +1,21 @@
+/*
+ * Descripción:Se pide que se soliciten y almacenen datos al usuario, y que se le aplican ciertas funciones que después deberán ser explicadas
+ * Autor: Izan Rico Blanco
+ * Fecha: 01/10/2025
+ */
+
+package Ejercicio4;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		double datoUsuario;
+		
+		
+
+	}
+
+}
