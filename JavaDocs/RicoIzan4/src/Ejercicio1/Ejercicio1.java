4cc273ca8ffcbcea88d8691f642bce285735cd0e
Izan <iricbla2402@g.educaand.es>
2025-10-08 21:25:56 +0200

    Inicio de la practica 4

---
+++ JavaDocs/RicoIzan4/src/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,34 @@
+/*
+ * Descripción: Pedirle al usuario dos valores y compararlas. Por último se pide que se le informe al usuario
+ * Autor: Izan Rico Blanco
+ * Fecha: 08/10/2025
+ */
+
+package Ejercicio1;
+
+import java.util.Scanner;
+
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		double num1;
+		double num2;
+		
+		System.out.println("Por favor, ingresa el primer valor: ");
+		num1 = teclado.nextDouble();
+		System.out.println("Por favor, ingresa el segundo valor: ");
+		num2 = teclado.nextDouble();
+		
+		if(num1 < num2) {
+			System.out.println("El primer valor el más pequeño que el segundo.");
+			
+		}else if {
+			
+		}
+	}
+
+}
