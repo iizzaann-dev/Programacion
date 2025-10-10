656d57af31943c839edca92c7d542587505f37d9
Izan <iricbla2402@g.educaand.es>
2025-10-10 19:43:09 +0200

    Eliminación de carpetas innecesarias y subida del ejercicio 2 de la practica 4

--- JavaDocs/RicoIzan4/src/Ejercicio1/Ejercicio1.java
+++ JavaDocs/RicoIzan4/src/Ejercicio1/Ejercicio1.java
@@ -14,20 +14,22 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner(System.in);
-		
+																	//Declaramos variables
 		double num1;
 		double num2;
 		
-		System.out.println("Por favor, ingresa el primer valor: ");
+		System.out.println("Por favor, ingresa el primer valor: "); //Pedimos los valores de las variables
 		num1 = teclado.nextDouble();
 		System.out.println("Por favor, ingresa el segundo valor: ");
 		num2 = teclado.nextDouble();
 		
 		if(num1 < num2) {
-			System.out.println("El primer valor el más pequeño que el segundo.");
+			System.out.println("El primer valor es más pequeño que el segundo.");//Mostramos en pantalla la comparación entre los valores ingresados
 			
-		}else if {
-			
+		}else if (num1 > num2){
+			System.out.println("El primer valor es más grande que el segundo.");
+		}else {
+			System.out.println("Los valores ingresados son iguales.");
 		}
 	}
 
