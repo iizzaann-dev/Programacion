3f728b4bdfb267f3eeb8390f48d88e1192d4a077
Izan <iricbla2402@g.educaand.es>
2025-10-16 11:43:53 +0200

    Subida del ejercicio 5 de la práctica 6

---
+++ JavaDocs/RicoIzan6/src/Ejercicio5/Ejercicio5.java
@@ -0,0 +1,16 @@
+/*
+ * Descripción: 
+ * Autor: Izan Rico Blanco
+ * Fecha: 15/10/2025
+ */
+
+package Ejercicio5;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
