906b53d6d7d4a6868de6b0eb6ed5d3ed992e895c
Izan <iricbla2402@g.educaand.es>
2025-10-16 18:16:09 +0200

    Subida de parte del ejercicio 5

--- JavaDocs/RicoIzan6/src/Ejercicio5/Ejercicio5.java
+++ JavaDocs/RicoIzan6/src/Ejercicio5/Ejercicio5.java
@@ -1,16 +1,31 @@
 /*
- * Descripción: 
+ * Descripción: Se le piden 10 números al usuario, se hace la media de los números positivos, la media de los números negativos y la cantidad de 0 ingresados.
  * Autor: Izan Rico Blanco
  * Fecha: 15/10/2025
  */
 
 package Ejercicio5;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
 		
-
+		int num1,num2,num3,num4,num5,num6,num7,num8,num9,num10;
+		
+		System.out.println("Por favor, ingresa el primer número: ");
+		
+		System.out.println("Por favor, ingresa el primer número: ");
+		System.out.println("Por favor, ingresa el primer número: ");
+		System.out.println("Por favor, ingresa el primer número: ");
+		System.out.println("Por favor, ingresa el primer número: ");
+		System.out.println("Por favor, ingresa el primer número: ");
+		System.out.println("Por favor, ingresa el primer número: ");
+		System.out.println("Por favor, ingresa el primer número: ");
+		System.out.println("Por favor, ingresa el primer número: ");
+		System.out.println("Por favor, ingresa el primer número: ");
 	}
 
 }
