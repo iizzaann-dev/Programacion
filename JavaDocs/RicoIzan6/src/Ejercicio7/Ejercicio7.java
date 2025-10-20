59f24851779f341367800113e286f12d0eab105f
Izan <iricbla2402@g.educaand.es>
2025-10-20 20:24:58 +0200

    Subida del ejercicio 7

--- JavaDocs/RicoIzan6/src/Ejercicio7/Ejercicio7.java
+++ JavaDocs/RicoIzan6/src/Ejercicio7/Ejercicio7.java
@@ -1,9 +1,38 @@
+/*
+ * Descripción: Se calcula un número aleatorio entre 0 y 50 y se le pide al usuario que ingrese números hasta que lo acierte.
+ * Autor: Izan Rico Blanco
+ * Fecha: 20/10/2025
+ */
+
 package Ejercicio7;
+
+import java.util.Scanner;
+
 
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner (System.in);
+		
+		
+		int numRandom, entradaUsuario;
+		
+		numRandom = (int)(Math.random() * 50);
+		System.out.println("Ingresa un número: ");
+		entradaUsuario = teclado.nextInt();
+		
+		
+		while (entradaUsuario != numRandom) {
+			if (entradaUsuario < numRandom) {
+				System.out.println("Ingresa un número más alto");
+				entradaUsuario = teclado.nextInt();
+				
+			}else {
+				System.out.println("Ingresa un número más pequeño");
+				entradaUsuario = teclado.nextInt();
+			}
+		}
+		System.out.println("¡Correcto!");
 
 	}
 
