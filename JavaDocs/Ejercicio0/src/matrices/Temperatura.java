393ab5f104c84bc0372f7343e84c77463dfa7f2d
Izan <iricbla2402@g.educaand.es>
2025-10-22 21:12:48 +0200

    Subida del ejercicio de ejemplo de los arrays bidimensionales

---
+++ JavaDocs/Ejercicio0/src/matrices/Temperatura.java
@@ -0,0 +1,53 @@
+/*
+ * 
+ */
+
+package matrices;
+
+public class Temperatura {
+
+	public static void main(String[] args) {
+		final int TOTAL_MESES = 12; 
+		final int TOTAL_DIAS =30;
+		int diaMasCalor = 1, tempMasAlto, diaTemp10;
+		int[][] temperatura;  											//Declaramos un array bidimensional
+		temperatura = new int[TOTAL_MESES][TOTAL_DIAS];   
+																		//Inicialización
+		for(int mes=0; mes< temperatura.length; mes++) {				//Nos movemos por los meses
+			for(int dia = 0; dia < temperatura[mes].length; dia++) {							//Nos movemos por los días
+				temperatura[mes][dia] = (int)(Math.random() * 40);
+				
+			}
+		}
+		for(int mes=0; mes<temperatura.length; mes++) {				
+			for(int dia = 0; dia < temperatura[mes].length; dia++) {		
+				System.out.print(temperatura[mes][dia] +"\t");
+			}
+			System.out.println();
+		}
+		
+		for(int mes = 0; mes < temperatura.length; mes++) {
+			tempMasAlto = temperatura[mes][0];  						//Cada mes inicializo la temperatura más alta a la temperatura del primer dia
+			for(int dia = 0; dia < temperatura[mes].length; dia++) {							//Empiezo a comparar a partir del segundo 
+				if (tempMasAlto < temperatura[mes][dia]) {
+					tempMasAlto = temperatura[mes][dia];
+					diaMasCalor = dia;
+				}
+			}
+			System.out.println("El día " + diaMasCalor + " hubo " + tempMasAlto + " grados.");
+		}
+		
+		//Para cada mes, mostrar el primer dia con temperatura inferiror a los 10 grados. 
+		
+		for (int mes = 0; mes < temperatura.length; mes++) {
+			int dia = 0;
+			diaTemp10 = 0;
+			do {
+				diaTemp10++;
+			}while(temperatura[mes][diaTemp10] > 10);
+			System.out.println("Para el mes " + mes + " el primer día con menos de 10 grados es el "+ diaTemp10 + " día.");
+		}
+		
+	}
+}
+
