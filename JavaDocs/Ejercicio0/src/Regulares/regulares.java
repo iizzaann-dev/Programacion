902e0dd202e0c3de4a072f003001930c82041775
Izan <iricbla2402@g.educaand.es>
2025-10-30 18:12:39 +0100

    regulares

---
+++ JavaDocs/Ejercicio0/src/Regulares/regulares.java
@@ -0,0 +1,15 @@
+package Regulares;
+
+import java.util.Scanner;
+
+public class regulares {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		
+		//[0-9]{8}[A-Z]    <---------- Para el DNI
+		//[0-9]{4}[A-Z]{3} <---------- Para la matrícula
+	}
+
+}
