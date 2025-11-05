6e3a709a39f980759508d7f1f861f2c20a3a8f93
Izan <iricbla2402@g.educaand.es>
2025-11-05 20:28:09 +0100

    Inicio del ejercicio 5

---
+++ JavaDocs/RicoIzan3.2/src/Ejercicio5/Ejercicio5.java
@@ -0,0 +1,31 @@
+/*
+ * Descripción: Necesitamos crear un programa para registrar sueldos de hombres y mujeres de una
+ * empresa y detectar si existe brecha salarial entre ambos. El programa pedirá por teclado la
+ * información de N personas distintas (valor también introducido por teclado). Para cada
+ * persona, pedirá su género (0 para varón y 1 para mujer) y su sueldo. Esta información debe
+ * guardarse en una única matriz. Luego se mostrará por pantalla el sueldo medio de cada
+ * género.
+ * Autor: Izan Rico Blanco
+ * Fecha: 31/10/2025
+ */
+
+
+package Ejercicio5;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		double brecha [][];
+		double genero = 0;
+		double sueldo = 0;
+		brecha = new double [genero][sueldo];
+		System.out.println("Por favor, ingresa tu genero (0 para varón y 1 para mujer): ");
+
+
+	}
+
+}
