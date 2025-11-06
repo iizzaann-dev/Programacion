84955f36aa14fb9650b3f747451407fb53d4671a
Izan <iricbla2402@g.educaand.es>
2025-11-06 17:38:27 +0100

    Subida del ejercicio 1 de la practica 3.12

--- JavaDocs/RicoIzan3.12/src/Ejercicio1/Ejercicio1.java
+++ JavaDocs/RicoIzan3.12/src/Ejercicio1/Ejercicio1.java
@@ -1,7 +1,8 @@
 /*
- * Descripción: 
+ * Descripción: Crea un programa que pida una cadena de texto por teclado y luego muestre cada palabra
+ * de la cadena en una línea distinta
  * Autor: Izan Rico Blanco
- * Fecha: 
+ * Fecha: 03/11/2025
  */
 
 
@@ -13,7 +14,18 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
+		
+		
+		System.out.println("Ingresa una cadena de carácteres: ");
 
+		String cadena = teclado.nextLine();
+		
+		for (int contador = 0; contador < cadena.length(); contador++) {
+			char letras = cadena.charAt(contador);
+			System.out.println(letras);
+
+		}
+		
 	}
 
 }
