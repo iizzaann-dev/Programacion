0a7fa109fd3edabe78601154f71cbbd62d4b3c3a
Izan <iricbla2402@g.educaand.es>
2025-11-06 17:21:08 +0100

    Varios metodos en un solo ejercicio

--- JavaDocs/Ejercicio0/src/PrimerMetodo/MetodosVarios.java
+++ JavaDocs/Ejercicio0/src/PrimerMetodo/MetodosVarios.java
@@ -3,12 +3,19 @@ package PrimerMetodo;
 public class MetodosVarios {
 
 	public static void main(String[] args) {
-		
+		 boolean par; 
+		 par = esPar(8);
+		 System.out.println(par);
+		 
+		 System.out.println(esPar(9));
+		 
+		 System.out.println(esImpar(9));
+
 
 	}
 	
 	// Calcula si numero es par devolviendo un valor booleano
-	public static void espar(int numero) {
+	public static boolean esPar(int numero) {
 		boolean par ;
 		
 		if (numero % 2 == 0) {
@@ -16,5 +23,11 @@ public class MetodosVarios {
 		}else {
 			par = false;
 		}
+		return par;
+	}
+	// Calcula si numero es impar devolviendo un valor booleano
+	public static boolean esImpar(int numero) {
+		
+		return !esPar(numero);
 	}
 }
