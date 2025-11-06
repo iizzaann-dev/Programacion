846b967aa6de972f7eac3fca49a2857f8d6e3396
Izan <iricbla2402@g.educaand.es>
2025-11-06 17:03:49 +0100

    Creando el primer metodo

---
+++ JavaDocs/Ejercicio0/src/PrimerMetodo/MetodosVarios.java
@@ -0,0 +1,14 @@
+package PrimerMetodo;
+
+public class MetodosVarios {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	// Calcula si numero es par devolviendo un valor booleano
+	public static void espar(int numero) {
+		
+	}
+}
