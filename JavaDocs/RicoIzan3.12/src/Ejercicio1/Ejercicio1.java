f2935e221ea43a9571423e48f57d219e3528bab8
Izan <iricbla2402@g.educaand.es>
2025-11-06 16:32:38 +0100

    creacion del proyecto 3.12

---
+++ JavaDocs/RicoIzan3.12/src/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,19 @@
+/*
+ * Descripción: 
+ * Autor: Izan Rico Blanco
+ * Fecha: 
+ */
+
+
+package Ejercicio1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+	}
+
+}
