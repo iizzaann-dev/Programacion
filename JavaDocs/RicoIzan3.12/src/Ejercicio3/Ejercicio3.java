d8dc2677b90ab7c6d289330e384920b55c5eb80c
Izan <iricbla2402@g.educaand.es>
2025-11-07 20:14:02 +0100

    Subida del ejercicio 3 de la práctica 3.12

---
+++ JavaDocs/RicoIzan3.12/src/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,49 @@
+/*
+ * Descripción: Crea un programa que pida por teclado tres cadenas de texto: nombre y dos apellidos.
+ * Luego mostrará un código de usuario (en mayúsculas) formado por la concatenación de
+ * las tres primeras letras de cada uno de ellos. Por ejemplo si se introduce “Lionel”, “Tarazón” y
+ * “Alcocer” mostrará “LIOTARALC”.
+ * Autor: Izan Rico Blanco
+ * Fecha: 07/11/2025
+ */
+
+package Ejercicio3;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		
+		String cadena1, cadena2, cadena3, nombre, apellido1, apellido2;
+		
+		System.out.println("Por favor, ingresa tu nombre: ");
+		nombre = teclado.nextLine();
+		
+		System.out.println("Por favor, ingresa tu primer apellido: ");
+		apellido1 = teclado.nextLine();
+		
+		System.out.println("Por favor, ingresa tu segundo apellido: ");
+		apellido2 = teclado.nextLine();
+		
+		cadena1 = extraerLetras(nombre);
+		cadena2 = extraerLetras(apellido1);
+		cadena3 = extraerLetras(apellido2);
+		
+		System.out.println("Tu codigo de usuario es: " + cadena1 + cadena2 + cadena3);
+
+
+
+	}
+
+	public static String extraerLetras (String letras) {
+		String letra = "";
+		letra.toUpperCase();
+		letra = letras.substring(0,3);
+		return letra;
+	}
+}
+
+
