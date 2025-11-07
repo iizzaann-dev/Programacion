15292f4c893328ce3542bad5a86f48f05a6e53da
Izan <iricbla2402@g.educaand.es>
2025-11-07 19:51:26 +0100

    Subida del ejercicio 2 de la práctica 3.12

--- JavaDocs/RicoIzan3.12/src/Ejercicio2/Ejercicio2.java
+++ JavaDocs/RicoIzan3.12/src/Ejercicio2/Ejercicio2.java
@@ -17,14 +17,22 @@ public class Ejercicio2 {
 		
 		String cadena1, cadena2;
 		
-		System.out.println("Por favor, ingresa una cadena de caracteres: ");
+		System.out.println("Por favor, ingresa una cadena de caracteres: ");		//Pedimos la primera cadena de texto
 		cadena1 = teclado.nextLine();
 		
-		System.out.println("De acuerdo, ahora ingresa otra cadena de caracteres: ");
+		System.out.println("De acuerdo, ahora ingresa otra cadena de caracteres: ");//Pedimos la segunda cadena de texto
 		cadena2 = teclado.nextLine();
 		
-		if (cadena1.equalsIgnoreCase(cadena2) == true) {
+		if (cadena1.equalsIgnoreCase(cadena2)) {									//Si son iguales sin importar las mayus o minus
+			System.out.println("Son iguales sin diferenciar entre mayúsculas y minúscualas.");
 			
+			if(cadena1.equals(cadena2)) {											//Si son iguales teniendo en cuenta las mayúsculas y minúsculas
+				System.out.println("Son iguales diferenciando entre mayúsculas y minúscualas.");
+			}
+
+		}else {																		//Si no son iguales desde un inicio
+			System.out.println("No son iguales, ni diferenciando ni sin diferenciar entre mayúsculas y minúsculas.");
+
 		}
 		
 
