83de20bc9ed8585a43a9373976083c141ee4eb81
Izan <iricbla2402@g.educaand.es>
2025-11-13 17:08:33 +0100

    Ejercicio 1 relacion 4.14

---
+++ JavaDocs/RicoIzan4.14/src/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,33 @@
+/*
+ * Descripción: Escribe un programa que pida dos números reales por teclado y muestre por pantalla el
+ * resultado de multiplicarlos. Implementa y utiliza la función:
+ * double multiplica(double a, double b) // Devuelve la multiplicación de dos números
+ * Autor: Izan Rico Blanco
+ * Fecha: 13/11/2025
+ */
+
+package Ejercicio1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		double num1 = 0, num2 = 0;											//Declaramos las variables
+		
+		System.out.println("Ingresa el valor del primer número: ");
+		num1 = teclado.nextDouble();										//Pedimos el valor de la primera variable
+		
+		System.out.println("Ingresa el valor del segundo número: ");
+		num2 = teclado.nextDouble();										//Pedimos el valor de la segunda variable
+		
+		System.out.printf("El valor de la multiplicación de %.0f y %.0f es: %.0f" ,num1, num2, multiplica(num1, num2));
+			//Usamos printf para que sea mas sencillo la impresion de datos
+	}
+	
+	public static double multiplica(double a, double b) {
+		return a * b;
+	}
+}
