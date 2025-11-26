f7287ee1087f5bcfecda57355565a1440dd4cfbe
Izan Rico Blanco <iricbla2402@g.educaand.es>
2025-11-26 19:27:06 +0100

    EJERCICIO 3 DE LA RELACION 4.15

---
+++ JavaDocs/RicoIzan4.15/src/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,19 @@
+/*
+ *Descripción:
+ *Nombre: Izan Rico Blanco
+ *Fecha: 26/11/2025
+*/
+
+
+package Ejercicio3;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+	}
+
+}
