78c3451c64c1baeddff5443f0f5591493893630b
Izan Rico Blanco <iricbla2402@g.educaand.es>
2025-11-28 21:25:07 +0100

    OBJETOS :(

--- JavaDocs/RicoIzan4.16/src/Ejercicio1/Ejercicio1.java
+++ JavaDocs/RicoIzan4.16/src/Ejercicio1/Ejercicio1.java
@@ -1,10 +1,48 @@
 package Ejercicio1;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+import libtarea3.CuentaBancaria;
+
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//1
+		CuentaBancaria cuentaConjunta, cuentaPrivada, cuentaFamiliar;
+		
+		//2
+		
+			//2.1
+		
+			CuentaBancaria cuenta1; 
+			
+			double saldoInicial = 0;
+			LocalDate fechaCreacion = LocalDate.of(2027, 9, 1);
+			
+			try {
+				cuenta1 = new CuentaBancaria(saldoInicial, fechaCreacion);
+				
+			} catch (DateTimeException excepcionFecha) {
+				System.out.printf("No se puede crear una cuenta de banco en la fecha %S", fechaCreacion);
+			} catch (IllegalArgumentException excepcionArgumentoIlegal) {
+				System.out.printf("No se puede crear una cuenta de banco en la fecha %S", fechaCreacion);
+			}
+			
+				
+			
+			
+			
+			
+			
+			
+			cuentaConjunta = new CuentaBancaria();
+			cuentaPrivada = new CuentaBancaria();
+			cuentaFamiliar = new CuentaBancaria();
+		
+		
 	}
 
 }
