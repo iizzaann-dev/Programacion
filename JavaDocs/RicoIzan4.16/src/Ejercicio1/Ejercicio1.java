734d66688316244dbd758213ffc767e0a21394c1
Izan Rico Blanco <iricbla2402@g.educaand.es>
2025-12-01 21:26:35 +0100

    Ejercicio 1 objetos

--- JavaDocs/RicoIzan4.16/src/Ejercicio1/Ejercicio1.java
+++ JavaDocs/RicoIzan4.16/src/Ejercicio1/Ejercicio1.java
@@ -16,14 +16,12 @@ public class Ejercicio1 {
 		//2
 		
 			//2.1
-		
-			CuentaBancaria cuenta1; 
 			
 			double saldoInicial = 0;
 			LocalDate fechaCreacion = LocalDate.of(2027, 9, 1);
 			
 			try {
-				cuenta1 = new CuentaBancaria(saldoInicial, fechaCreacion);
+				cuentaConjunta = new CuentaBancaria(saldoInicial, fechaCreacion);
 				
 			} catch (DateTimeException excepcionFecha) {
 				System.out.printf("No se puede crear una cuenta de banco en la fecha %S", fechaCreacion);
@@ -31,18 +29,58 @@ public class Ejercicio1 {
 				System.out.printf("No se puede crear una cuenta de banco en la fecha %S", fechaCreacion);
 			}
 			
-				
+			System.out.println("");
+
+			//2.2
+			
+			LocalDate fecha2 = LocalDate.now();
+			saldoInicial = -200;
+			try {
+				cuentaConjunta = new CuentaBancaria(saldoInicial, fecha2);
+			} catch (java.lang.IllegalArgumentException excepcionArgumentoIlegal) {
+				System.out.printf("No se puede crear una cuenta de banco con un saldo inicial de %.0f.", saldoInicial );
+			}
+			
+			System.out.println("");
+
+			
+			//2.3
+
+			LocalDate fecha3 = LocalDate.of(2021, 7, 1);
+			saldoInicial = 1000;
+			double limiteDescubierto= -200;
+			
+			cuentaPrivada = new CuentaBancaria(saldoInicial, fecha3, limiteDescubierto);
+			
+
+			System.out.println("");
 			
 			
+			//2.4
+			LocalDate fecha4 = LocalDate.of(2021, 7, 1);
+			saldoInicial = 200;
+
+			cuentaConjunta = new CuentaBancaria(saldoInicial, fecha4);
+		
 			
+			//2.5
 			
-			
-			
-			cuentaConjunta = new CuentaBancaria();
-			cuentaPrivada = new CuentaBancaria();
 			cuentaFamiliar = new CuentaBancaria();
-		
-		
+			
+			
+			//3
+			
+			System.out.printf("El identificador de la cuenta privada es: %s \n", cuentaPrivada.getId());
+			System.out.printf("La fecha de creación de la cuenta privada es: %s \n", cuentaPrivada.getFechaCreacion());
+			System.out.printf("El limite descubierto de la cuenta privada es: %.2f \n", cuentaPrivada.getLimiteDescubierto());
+			System.out.printf("Estado de la cuenta (embargada o no): %b \n", cuentaPrivada.isEmbargada());
+			System.out.printf("Estado de la cuenta (descubierta o no): %b \n", cuentaPrivada.isDescubierta());
+			System.out.printf("Antigüedad de la cuenta: %d días \n", cuentaPrivada.getDiasCuenta());
+			
+			
+			//4
+			
+			
 	}
 
 }
