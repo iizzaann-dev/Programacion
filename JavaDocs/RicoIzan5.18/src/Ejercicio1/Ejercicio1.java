a30ac324633546fcf2db125898295cb1a29d0154
Izan Rico Blanco <iricbla2402@g.educaand.es>
2025-12-15 20:11:53 +0100

    cambio de nombre al paquete de bombillas

---
+++ JavaDocs/RicoIzan5.18/src/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,17 @@
+/*
+ *Descripción: Ejercicio de las bombillas
+ *Nombre: Izan Rico Blanco
+ *Fecha: 15/12/2025
+*/
+
+package Ejercicio1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
