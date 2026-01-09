c1a53ec19c3cb5b427bdd83f1389020c26564f73
Izan Rico Blanco <iricbla2402@g.educaand.es>
2026-01-09 21:21:12 +0100

    Creando un constructor para una clase

--- JavaDocs/RicoIzan6.19/src/Persona/Alumno.java
+++ JavaDocs/RicoIzan6.19/src/Persona/Alumno.java
@@ -2,27 +2,62 @@ package Persona;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
-public class Alumno {
+public class Alumno  {
 		
-	public static final short MaxAlumnosClase = 0;
-	public enum NivelIngles { A1, A2, B1, B2, C1, C2 };
-
+	public static final short MaxAlumnosInstituto = 1500;
+	public static final LocalTime horaMaxGeneral = LocalTime.of(21,18);
+	public static final short numMaxAlumnos = 30; 
+	public static final short numMinAlumnos = 10; 
 	
 	private String DNI;
 	private String Nombre;
 	private LocalDate fechaNacimiento;
-	private byte numHermanos;
-	private short anioComienzo;
+	private double peso;
+	private double altura;
+	private byte numHermanos;		
 	private short numHoras;
 	private static short numAlumnosMatriculados;
-	private LocalDateTime horaInicioContactar;
-	private LocalDateTime horaFinalContarcar;
-	private boolean hijos;
-	
-	private NivelIngles nivelIng;
-	
-	
+	private byte mayorEdad;
+	private LocalTime horaMaxAlumno;
 	
 
+	
+	public Alumno (String DNI, String nombre, String apellido1, String apellido2, LocalDate fechaNacimiento, double peso, double altura, byte numHermanos, short numHoras, short numAlumnosMatriculados, 
+			byte mayorEdad, LocalTime horaMaxAlumno) throws IllegalArgumentException {
+		
+		if (DNIreal(DNI) == false ) {
+			throw new IllegalArgumentException ("No se han ingrasdo valores correctos de un DNI.");
+		}else if (DNI.isEmpty()) {
+			throw new IllegalArgumentException ("No se ha ingresado ningún valor para el DNI.");
+		}
+		
+		if (nombre == null) {
+			throw new IllegalArgumentException ("No se ha ingrasdo un tipo de dato correcto.");
+		}else if (nombre.isEmpty()) {
+			throw new IllegalArgumentException ("No se ha ingresado un nombre para el alumno.");
+
+		}
+
+
+		
+	}
+	
+	public static boolean DNIreal(String DNI) {
+		
+		boolean resultado = false;
+		
+		if (!DNI.matches("^[0-9]{8}[A-Z]$")) {
+			resultado = false;
+			
+		}else if (DNI.isEmpty()) {
+			resultado = true;
+		}
+		return resultado;
+	}
+	
+	
 }
+
+	
