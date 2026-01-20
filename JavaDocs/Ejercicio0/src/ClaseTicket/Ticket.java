66b4f5725adc4a9e26271dd0baa462d95b41e2b9
Izan Rico Blanco <iricbla2402@g.educaand.es>
2026-01-20 19:09:30 +0100

    asfdas

--- JavaDocs/Ejercicio0/src/ClaseTicket/Ticket.java
+++ JavaDocs/Ejercicio0/src/ClaseTicket/Ticket.java
@@ -23,7 +23,24 @@ public class Ticket {
     // Atributos de objeto variables (estado)
     private LocalTime usado;  // Momento de uso del ticket
     
-    public Ticket() throws IllegalArgumentException{
-    	
+    
+    public Ticket(LocalDate fecha) throws IllegalStateException, IllegalArgumentException{
+    	  LocalDate hoy = LocalDate.now();
+    	  if (fecha == null) {
+    		  throw new IllegalArgumentException ("La fecha es invalida (es null).");
+    	  }
+    	  
+    	  if (fecha.isBefore(hoy)) {
+    		  throw new IllegalArgumentException ("El uso del ticket es anterior a hoy (%s)" + hoy);
+    	  }
+    	  
+    	  if (fecha.getYear() > hoy.getYear()) {
+    		  throw new IllegalArgumentException ("El uso del ticker es posteriror al año actual" + fecha);
+    	  }
     }
+    
+    
+    public Ticket() throws IllegalStateException {
+    	  this(LocalDate.now());
+    	}
 }
