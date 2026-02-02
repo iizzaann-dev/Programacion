3ec93c26ef26fb5ecd79a38219d3187f75240a7f
Izan Rico Blanco <iricbla2402@g.educaand.es>
2026-02-02 20:09:19 +0100

    espero que no haya ningun lio ni error

---
+++ JavaDocs/RicoIzan6.21/src/CD/CD.java
@@ -0,0 +1,19 @@
+package CD;
+
+import Cancion.Cancion;
+
+public class CD {
+	
+	private Cancion [] canciones;
+	private int contador;
+	
+	// el vector inicializarlo a null
+	public CD(){
+		
+	}
+	
+	public CD (CD otro CD) {
+		
+	}
+	
+}
