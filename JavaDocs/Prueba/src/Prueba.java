aa62916be19c3804e209afc231a3ce826bd95e7c
Izan <iricbla2402@g.educaand.es>
2026-02-27 16:36:25 +0000

    Ahora si funciona creo, solo falta arreglar el que se abra en el workspace y no en otra ventana

---
+++ JavaDocs/Prueba/src/Prueba.java
@@ -0,0 +1,11 @@
+public class Prueba {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int x = 5;
+
+        for (int contador = 0; contador < x; contador++){
+            System.out.println("El valor de contador es: " + contador);
+        }
+    }
+}
