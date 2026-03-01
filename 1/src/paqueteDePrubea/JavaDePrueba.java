f4ab7318e38af0710873482282813e08ebe1a439
Izan <iricbla2402@g.educaand.es>
2026-03-01 15:26:00 +0000

    la prueba funciona correctamente

---
+++ 1/src/paqueteDePrubea/JavaDePrueba.java
@@ -0,0 +1,11 @@
+package paqueteDePrubea;
+
+public class JavaDePrueba {
+    public static void main(String[] args) {   
+    System.out.println("Esto es un mensaje para ver si funciona");
+
+    for(int x = 0; x < 16; x++){
+        System.out.printf("El valor de x es %d\n", x);
+    }
+    }
+}
---
+++ 1/src/prueba.java
@@ -0,0 +1,7 @@
+
+public class prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Ahora debe de ir bien y no hay ningún error dios mio.");
+    }
+}
