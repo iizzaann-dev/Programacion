b929a4ae23e2acd9b4733aa988060a8575c1fef2
Izan <iricbla2402@g.educaand.es>
2026-03-25 13:47:53 +0100

    ejercicio 2 terminado al completo

--- JavaDocs/8.29/src/botanico/Ejercicio02.java
+++ JavaDocs/8.29/src/botanico/Ejercicio02.java
@@ -1,7 +1,9 @@
 package botanico;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 
 /** Ejercicio 2. Búsqueda de especies de plantas populares
  * @author Profesor
@@ -20,6 +22,7 @@ public class Ejercicio02 {
         ArrayList <String> plantas = new ArrayList<>(10);
         ArrayList <String> plantas2 = new ArrayList<>(10);
         LinkedHashSet<String> plantasPopulares = new LinkedHashSet<>(10);
+        Map <String, ArrayList<Integer>> posicionesPopulares = new HashMap<>();
 
         // Variables auxiliares
 
@@ -42,8 +45,7 @@ public class Ejercicio02 {
             plantas2.add(Utilidades.especiePlantaAleatoria());
         }
 
-        System.out.println("Las plantas de la primera lista son: " + plantas);
-        System.out.println("Las plantas de la segunda lista son: " + plantas2);
+
         /* También lo podemos hacer asi
         for (int i = 0; i < 10; i++) {
             lista1.add(Utilidades.especiePlantaAleatoria());
@@ -71,17 +73,17 @@ public class Ejercicio02 {
             }
         }*/
 
-        for (int i = 0; i < plantas.size(); i++){
-            if (plantas.get(i).equals(plantas2.get(i))){       //Plantas.get(i) funciona como el elemento que se va a comparar, no como la posicion
+        for (int i = 0; i < plantas.size(); i++) {
+            String planta = plantas.get(i);
+
+            if (plantas.get(i).equals(plantas2.get(i))) {       //Plantas.get(i) funciona como el elemento que se va a comparar, no como la posicion
                 plantasPopulares.add(plantas.get(i));          //Usamos plantas.get(i) en el add porque estamos añadiendo el objeto no la posicion
+
+                posicionesPopulares.putIfAbsent(planta, new ArrayList<>());
+                posicionesPopulares.get(planta).add(i);
             }
         }
 
-        if (plantasPopulares.isEmpty()){
-            System.out.println("No hay plantas populares.");
-        }else{
-            System.out.println("Las plantas populares son: " + plantasPopulares);
-        }
 
         // Recorremos a la vez las dos listas
         
@@ -89,7 +91,72 @@ public class Ejercicio02 {
         //----------------------------------------------
         //            Salida de resultados
         //----------------------------------------------
-        
+
+
+        if (plantasPopulares.isEmpty()){
+            System.out.println("No hay plantas populares.");
+            System.out.println("No hay posiciones populares.");
+
+        }else {
+
+            System.out.print("[");
+            for (int i = 0; i < plantas.size(); i++){
+                String planta = plantas.get(i);
+
+                if (plantasPopulares.contains(planta)){
+                    System.out.printf("*%s*", planta);
+                }else{
+                    System.out.print(planta);
+                }
+
+                if (i < plantas.size() - 1){
+                    System.out.print(", ");
+                }
+            }
+            System.out.print("]");
+
+            System.out.println("");
+
+            System.out.print("[");
+            for (int i = 0; i < plantas2.size(); i++){
+                String planta = plantas2.get(i);
+
+                if (plantasPopulares.contains(planta)){
+                    System.out.printf("*%s*", planta);
+                }else {
+                    System.out.print(planta);
+                }
+
+                if (i < plantas2.size() - 1){
+                    System.out.print(", ");
+                }
+            }
+            System.out.print("]");
+
+            System.out.println("");
+            /*
+            System.out.println("Las plantas populares son: " + plantasPopulares);
+
+            for (String i : plantas){
+                if (plantasPopulares.contains(i)){
+                    System.out.printf("*%s* \n", i);
+                }else{
+                    System.out.println("Las plantas de la primera lista son: " + plantas);
+                }
+            }
+
+            for (String i : plantas2){
+                if (plantasPopulares.contains(i)){
+                    System.out.printf("*%s*\n", i);
+                }else{
+                    System.out.println("Las plantas de la segunda lista son: " + plantas2);
+                }
+            }
+
+            */
+
+            System.out.println("Las posiciones populares son: " + posicionesPopulares);
+        }
 
     }
 }
