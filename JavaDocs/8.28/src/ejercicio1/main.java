24b2e39b92672f8dfba31c0fe70d06741fa3ecbb
Izan <iricbla2402@g.educaand.es>
2026-04-01 17:46:01 +0000

    asopla

--- JavaDocs/8.28/src/ejercicio1/main.java
+++ JavaDocs/8.28/src/ejercicio1/main.java
@@ -8,25 +8,25 @@ public class main {
 
         ArrayList <Atleta> lista = new ArrayList<>();
 
-        for (int contador = 0; contador <= 30; contador++){
-            String nombre = "atleta" + contador;
-            
-            while (atleta == null) {
-                int edad = (int) (Math.random() * 70);
-                double altura = 1.50 + Math.random() * 0.50;
-                Atleta atleta = null;
+        for (int contador = 0; contador <= 30; contador++) {
+        String nombre = "atleta" + contador;
 
-                try {
-                    atleta = new Atleta(nombre, edad, altura);
-                } catch (IllegalArgumentException error1) {
-                    System.out.println("Error: " + error1.getMessage() + " - Reintentando...");
-                }
+        Atleta atleta = null; 
+
+        while (atleta == null) {
+            int edad = (int) (Math.random() * 70);
+            double altura = 1.50 + Math.random() * 0.50;
+
+            try {
+                atleta = new Atleta(nombre, edad, altura);
+            } catch (IllegalArgumentException error1) {
+                System.out.println("Error: " + error1.getMessage() + " - Reintentando...");
             }
+        }
 
-            
-            lista.add(atleta);
-
-            System.out.println(atleta);
+        lista.add(atleta);
+        System.out.println(atleta);
+        
         }
     }
     
