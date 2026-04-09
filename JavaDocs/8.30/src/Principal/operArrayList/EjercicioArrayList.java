02170c604108794bd0c7c73d2b7ededd423e1e06
Izan Rico Blanco <iricbla2402@g.educaand.es>
2026-04-09 18:12:07 +0200

    vamos por el apartado 6 a medias

--- JavaDocs/8.30/src/Principal/operArrayList/EjercicioArrayList.java
+++ JavaDocs/8.30/src/Principal/operArrayList/EjercicioArrayList.java
@@ -22,9 +22,6 @@ public class EjercicioArrayList {
 
             Scanner teclado = new Scanner(notas1);                                   //Abrimos el scanner leyendo el archivo, no con System.in
 
-            //Apartado 2
-            System.out.println("El número de registros que contiene es: " + notas1.length());
-
 
             while (teclado.hasNextLine()){                                           //Mientras haya más líneas en el archivo el bucle se repite
 
@@ -56,10 +53,39 @@ public class EjercicioArrayList {
 
         listaAlumnos.sort(new ComparadorGrupo());
 
+        //Apartado 2
+        System.out.println("El número de registros que contiene es: " + listaAlumnos.size());
+
         for (Alumnos i : listaAlumnos){
             System.out.println("");
             System.out.println(i);
         }
+
+        //Apartado 4
+        Alumnos alumno1 = new Alumnos("1º ESO D", "Rosario", "López Muro", 3, 3, 4, 4, 3);
+        listaAlumnos.add(alumno1);
+        listaAlumnos.sort(new ComparadorGrupo());
+
+
+        //Apartado 5
+        Alumnos alumno2 = new Alumnos("1º ESO B", "Alba", "Duarte Durán",2, 3, 4, 1, 3);
+
+        double media = listaAlumnos.size() / 2.00;
+        int parteEntera = (int) media;
+        double decimal = media - parteEntera;
+        int mediaEntera = 0;
+
+
+        if (decimal >= 0.5){
+            mediaEntera = parteEntera + 1;
+        }else if (decimal < 0.5){
+            mediaEntera = parteEntera;
+        }
+
+        listaAlumnos.add(mediaEntera, alumno2);
+        listaAlumnos.sort(new ComparadorGrupo());
+
+        //Apartado 6
 
     }
 }
